d14d0a48b9ddc49ae5579fafdf31b8b1c6927060
kairo kairo <GMaina424@gmail.com>
2025-11-10 05:04:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9061063515-e5cf6",
+  "appId": "1:635258576791:web:617fe7a5c9d4967bc7aed2",
+  "apiKey": "AIzaSyAE1AtQb3VUG-ED3V6fF2pTl09a3wr8k4Y",
+  "authDomain": "studio-9061063515-e5cf6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "635258576791"
+};
